5826b3da8c01b3bee3ad46796f5bd9ab9d1c7c85
computersunplugged <dmat@computersunplugged.com.au>
2026-08-07 10:53:43 +1000

    Update index.js

--- src/pages/endpoint/MEM/list-templates/index.js
+++ src/pages/endpoint/MEM/list-templates/index.js
@@ -232,7 +232,7 @@ const Page = () => {
     size: 'lg',
   }
 
-  const simpleColumns = ['Apps']
+  const simpleColumns = ['displayName', 'isSynced', 'package', 'description', 'Type', 'usage']
 
   const filterList = [
     {
